66e929c7e014f2c010393f9014e8ef1fe70785ed
Yose Rafhael <yrafhael@my.wctc.edu>
2024-03-06 23:19:54 -0600

    [1.02] Creating selection for user - using while loop to interact with user - reading user choice

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Your Movies Library");
+
+        // creating main loop using while loop to interact with user
+        while (true)
+        {
+            // creating selection for user to choose
+            Console.WriteLine("\nYour selection:");
+            Console.WriteLine("1. Add new movie");
+            Console.WriteLine("2. View all movies");
+            Console.WriteLine("3. View all ratings");
+            Console.WriteLine("4. View all tags");
+            Console.WriteLine("5. View all links");
+            Console.WriteLine("Enter any key to exit");
+
+            // reading user choice 
+            string choice = Console.ReadLine();
+
+        }
+    }
+}
